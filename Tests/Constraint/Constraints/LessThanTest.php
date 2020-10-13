444fcb1d6f93c1ecf242b65e796fdc1c4ff88b61
Matthew Smeets <m.smeets@performation.com>
2020-10-13 18:11:05 +0200

    Allow numeric values for GreaterThan and LessThan

--- Tests/Constraint/Constraints/LessThanTest.php
+++ Tests/Constraint/Constraints/LessThanTest.php
@@ -20,14 +20,45 @@ class LessThanTest extends ConfiguredConstraintTestCase
         $this->expectException(InvalidOptionsException::class);
 
         new ParsleyAssert\LessThan([
-            'value' => '10',
+            'value' => [],
         ]);
     }
 
     public function testNormalization(): void
     {
+        // integer
         $constraint = new ParsleyAssert\LessThan([
             'value' => 10,
+        ]);
+
+        $this->assertSame([
+            'data-parsley-lt' => '10',
+            'data-parsley-lt-message' => 'Invalid.',
+        ], $constraint->normalize($this->normalizer));
+
+        // float to int
+        $constraint = new ParsleyAssert\LessThan([
+            'value' => 10.0,
+        ]);
+
+        $this->assertSame([
+            'data-parsley-lt' => '10',
+            'data-parsley-lt-message' => 'Invalid.',
+        ], $constraint->normalize($this->normalizer));
+
+        // floating
+        $constraint = new ParsleyAssert\LessThan([
+            'value' => 10.5,
+        ]);
+
+        $this->assertSame([
+            'data-parsley-lt' => '10.5',
+            'data-parsley-lt-message' => 'Invalid.',
+        ], $constraint->normalize($this->normalizer));
+
+        // string
+        $constraint = new ParsleyAssert\LessThan([
+            'value' => "10",
         ]);
 
         $this->assertSame([
